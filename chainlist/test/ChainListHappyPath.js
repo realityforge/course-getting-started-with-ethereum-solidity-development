e4154af39a4f464910202fa5d1a8d301b6a84e98
Peter Donald <peter@realityforge.org>
2018-02-11 12:51:51 +1100

    Convert to ES6

--- chainlist/test/ChainListHappyPath.js
+++ chainlist/test/ChainListHappyPath.js
@@ -1,22 +1,20 @@
 // Notice this is the same API as used in migrations
 // It essentially involves using the proxy defined by
 // Truffle
-var ChainList = artifacts.require('./ChainList.sol');
+const ChainList = artifacts.require('./ChainList.sol');
 
 contract('ChainList', function(accounts) {
   // accounts is defined by the truffle test framework
-  it('should be initialized with empty values', function() {
-    return ChainList.deployed().then(function(instance) {
-      return instance.getArticle();
-    }).then(function(data) {
-      var seller = data[0];
-      var name = data[1];
-      var description = data[2];
-      var price = data[3];
-      assert.equal(seller, 0x0);
-      assert.equal(name, '');
-      assert.equal(description, '');
-      assert.equal(price, 0);
-    });
+  it('should be initialized with empty values', async() => {
+    const instance = await ChainList.deployed();
+    const data = await instance.getArticle();
+    const seller = data[0];
+    const name = data[1];
+    const description = data[2];
+    const price = data[3];
+    assert.equal(seller, 0x0);
+    assert.equal(name, '');
+    assert.equal(description, '');
+    assert.equal(price, 0);
   });
 });
