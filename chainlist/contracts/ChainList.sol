8969e847c7791c9713db62c2888d50d4d7184b35
Peter Donald <peter@realityforge.org>
2018-02-16 09:18:41 +1100

    Expand the contract to support buy action and tests to the happy path

--- chainlist/contracts/ChainList.sol
+++ chainlist/contracts/ChainList.sol
@@ -17,6 +17,14 @@ contract ChainList {
     uint256 _price
   );
 
+  // Buyer+Seller is indexed which means clients can search/filter on these fields
+  event BuyArticleEvent (
+    address indexed _seller,
+    address indexed _buyer,
+    string _name,
+    uint256 _price
+  );
+
   function sellArticle(string _name, string _description, uint256 _price) public {
     seller = msg.sender;
     name = _name;
@@ -30,5 +38,29 @@ contract ChainList {
 
   function getArticle() public view returns (address, address, string, string, uint256) {
     return (seller, buyer, name, description, price);
+  }
+
+  // Buy an article
+  function buyArticle() public payable {
+    // Ensure that we have an article to sell
+    require(seller != 0x0);
+    // Ensure that we don't yet have a buyer
+    require(buyer == 0x0);
+    // Ensure that the buyer is not the seller
+    require(seller != msg.sender);
+    // Ensure that the amount sent is the price
+    require(msg.value == price);
+
+    // Record buyer
+    buyer = msg.sender;
+
+    // Pay the seller with the eth passed to this transaction
+    // This method will run "revert" if the transfer fails for any reason
+    // we could have used the "send" method on address which returns false if transfer fails
+    // but then we would have needed to check return value and execute revert outselves if it failed
+    seller.transfer(msg.value);
+
+    // Notify listeners
+    BuyArticleEvent(seller, buyer, name, price);
   }
 }
--- chainlist/test/ChainListHappyPath.js
+++ chainlist/test/ChainListHappyPath.js
@@ -6,6 +6,12 @@ const ChainList = artifacts.require('./ChainList.sol');
 contract('ChainList', function(accounts) {
   // accounts is defined by the truffle test framework
 
+  const seller = accounts[0];
+  const buyer = accounts[1];
+  const name = 'My iPhone';
+  const description = 'Good condition';
+  const price = Number(web3.toWei(1, 'ether'));
+
   it('should be initialized with empty values', async() => {
     const contract = await ChainList.deployed();
     const results = await contract.getArticle();
@@ -18,10 +24,6 @@ contract('ChainList', function(accounts) {
 
   it('should set values when sellArticle invoked', async() => {
     const contract = await ChainList.deployed();
-    const seller = accounts[0];
-    const name = 'My iPhone';
-    const description = 'Good condition';
-    const price = web3.toWei(1, 'ether');
     /*
      * The receipt includes the events as well as other details about the transaction.
      */
@@ -38,6 +40,43 @@ contract('ChainList', function(accounts) {
     const results = await contract.getArticle();
     assert.equal(results[0], seller, 'seller');
     assert.equal(results[1], 0x0, 'buyer');
+    assert.equal(results[2], name, 'name');
+    assert.equal(results[3], description, 'description');
+    assert.equal(results[4], price, 'price');
+  });
+
+  it('should be able to buy Article', async() => {
+    const contract = await ChainList.deployed();
+
+    // Just be aware that the state of the block chain is flowing through from earlier test ....
+    // boo - state is not clean but at least it means we can buy the article already setup
+
+    const sellerBalanceBeforeBuy = web3.eth.getBalance(seller).toNumber();
+    const buyerBalanceBeforeBuy = web3.eth.getBalance(buyer).toNumber();
+    /*
+     * The receipt includes the events as well as other details about the transaction.
+     */
+    const receipt = await contract.buyArticle({ from: buyer, value: price });
+
+    assert.equal(receipt.logs.length, 1, 'One event should have been triggered');
+
+    const event = receipt.logs[0];
+    assert.equal(event.event, 'BuyArticleEvent');
+    assert.equal(event.args._seller, seller);
+    assert.equal(event.args._buyer, buyer);
+    assert.equal(event.args._name, name);
+    assert.equal(event.args._price, price);
+
+    const sellerBalanceAfterBuy = web3.eth.getBalance(seller).toNumber();
+    const buyerBalanceAfterBuy = web3.eth.getBalance(buyer).toNumber();
+
+    assert.equal(sellerBalanceAfterBuy, sellerBalanceBeforeBuy + Number(price), 'Seller should have got the ETH from transaction');
+    assert(buyerBalanceAfterBuy <= buyerBalanceBeforeBuy - Number(price),
+      'Buyer should have paid the ETH for transaction + gas');
+
+    const results = await contract.getArticle();
+    assert.equal(results[0], seller, 'seller');
+    assert.equal(results[1], buyer, 'buyer');
     assert.equal(results[2], name, 'name');
     assert.equal(results[3], description, 'description');
     assert.equal(results[4], price, 'price');
